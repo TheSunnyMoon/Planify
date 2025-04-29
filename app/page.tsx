c7a2bafe68835acafa4e3c3c65e15802e8beb880
mathis <mathismayo@outlook.com>
2025-04-29 10:57:59 +0200

    init

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { LoginForm } from "@/components/LoginForm";
+
+export default function Home() {
+  return (
+    <div> 
+        <LoginForm />
+    </div>
+
+  );
+}
---
+++ components/Login.tsx
@@ -0,0 +1,6 @@
+
+export const Login = (Username:string , Password: string ) => {
+    return (
+        
+    );
+}
